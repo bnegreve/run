6fb348c39eca284e084da95b2cfe4a049e9d85a1
Benjamin Negrevergne <Benjamin.Negrevergne@imag.fr>
2012-04-02 14:39:52 +0200

    added a post execution script command

--- use_mem.c
+++ use_mem.c
@@ -17,12 +17,15 @@ int main(int argc, char **argv){
     fprintf(stderr, "%s <how much mem in mb> <for how long in s>\n", argv[0]); 
     return EXIT_FAILURE; 
   }
+
+
   size_t how_much = atoi(argv[1]);
   how_much = how_much>MAX_MEM?MAX_MEM:how_much;
   how_much<<= 20; //convert to bytes
 
   size_t how_long = atoi(argv[2]);
 
+  printf("%d-%d\n", how_much, how_long); 
   char *q, *p = malloc(how_much); 
   
   for(q = p; q != p+how_much; q++){
